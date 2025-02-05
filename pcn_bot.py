7b69211308aebba42ae652f9503fdc9c27a24f31
WindyP2504 <phongvu252001@gmail.com>
2025-02-05 15:09:46 +0700

    update_function

--- pcn_bot.py
+++ pcn_bot.py
@@ -1,6 +1,6 @@
 import psycopg2
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
 import re
 from datetime import datetime, timedelta
 
@@ -13,6 +13,23 @@ def connect_db():
         port="5432"  # Cổng mặc định của PostgreSQL
     )
     return conn
+
+def update_new_toDb(conn, cursor, no_off, message_text, first_name, last_name, username, user_id, today):
+    _is_sap = 0
+    if "sap" in message_text.lower() or "nghỉ phép" in message_text.lower():
+        _is_sap = 1
+        # Gửi phản hồi
+        reply_text = f"BOT đã ghi nhận {no_off} ngày nghỉ có phép từ {today.date()} cho {first_name} {last_name} ({username})."
+    else:
+        # Gửi phản hồi
+        reply_text = f"BOT đã ghi nhận {no_off} ngày nghỉ không phép từ {today.date()} cho {first_name} {last_name} ({username})."
+
+    query = """INSERT INTO public."PCN_work_time" ("UserId", "Username", "Year", "Month", "Day", "Date", "No_off", "Is_sap") VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+    cursor.execute(query, (user_id, username, today.year, today.month, today.day, today, no_off, _is_sap))
+    conn.commit()
+
+    # Trả về chuỗi reply_text
+    return reply_text
 
 def ensure_user_exists(cursor, user_id, username, first_name, last_name):
     cursor.execute("""SELECT COUNT(*) FROM public."PCN_employees" WHERE "UserId" = %s""", (user_id,))
@@ -36,8 +53,6 @@ def echo(update: Update, context: CallbackContext) -> None:
     conn = connect_db()
     cursor = conn.cursor()
 
-    is_sap = 0
-    no_off = 0
     today = datetime.now()
 
     try:
@@ -48,32 +63,39 @@ def echo(update: Update, context: CallbackContext) -> None:
             match = re.search(r"nghỉ (\d{1,2}(\.\d{1,2})?) ngày", message_text.lower())
             if match:
                 no_off = float(match.group(1))
-            elif "hôm nay" in message_text.lower():
-                no_off = 1
-            elif "ngày mai" in message_text.lower():
-                no_off = 1
-                today = datetime.now() + timedelta(days=1)
-            elif "sáng mai" in message_text.lower() or "chiều mai" in message_text.lower():
-                no_off = 0.5
-                today = datetime.now() + timedelta(days=1)
-            elif any(phrase in message_text.lower() for phrase in ["sáng nay", "chiều nay"]):
-                no_off = 0.5
+                update.message.reply_text(update_new_toDb(conn, cursor, no_off, message_text, first_name,last_name,username,user_id,today,))
+            else:
+                if "hôm nay" in message_text.lower():
+                    no_off = 1
+                    update.message.reply_text(update_new_toDb(conn, cursor, no_off, message_text, first_name,last_name,username,user_id,today,))
+                if "ngày mai" in message_text.lower():
+                    no_off = 1
+                    today = datetime.now() + timedelta(days=1)
+                    update.message.reply_text(update_new_toDb(conn, cursor, no_off, message_text, first_name,last_name,username,user_id,today,))
+                if "sáng mai" in message_text.lower() or "chiều mai" in message_text.lower():
+                    no_off = 0.5
+                    today = datetime.now() + timedelta(days=1)
+                    update.message.reply_text(update_new_toDb(conn, cursor, no_off, message_text, first_name,last_name,username,user_id,today,))
+                if any(phrase in message_text.lower() for phrase in ["sáng nay", "chiều nay"]):
+                    no_off = 0.5
+                    update.message.reply_text(update_new_toDb(conn, cursor, no_off, message_text, first_name,last_name,username,user_id,today,))
 
-            if no_off > 0:
-                if "sap" in message_text.lower() or "nghỉ phép" in message_text.lower():
-                    is_sap = 1
-                    # Gửi phản hồi
-                    update.message.reply_text(f"BOT đã ghi nhận {no_off} ngày nghỉ có phép cho {first_name} {last_name} ({username}).")
-                else:
-                    # Gửi phản hồi
-                    update.message.reply_text(f"BOT đã ghi nhận {no_off} ngày nghỉ không phép cho {first_name} {last_name} ({username}).")
+            # if no_off > 0:
+            #     if "sap" in message_text.lower() or "nghỉ phép" in message_text.lower():
+            #         is_sap = 1
+            #         # Gửi phản hồi
+            #         update.message.reply_text(f"BOT đã ghi nhận {no_off} ngày nghỉ có phép cho {first_name} {last_name} ({username}).")
+            #     else:
+            #         # Gửi phản hồi
+            #         update.message.reply_text(f"BOT đã ghi nhận {no_off} ngày nghỉ không phép cho {first_name} {last_name} ({username}).")
+            #
+            # query = """
+            # INSERT INTO public."PCN_work_time" ("UserId", "Username", "Year", "Month", "Day", "Date", "No_off", "Is_sap")
+            # VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            # """
+            # cursor.execute(query, (user_id, username, today.year, today.month, today.day, today, no_off, is_sap))
+            # conn.commit()
 
-            query = """
-            INSERT INTO public."PCN_work_time" ("UserId", "Username", "Year", "Month", "Day", "Date", "No_off", "Is_sap")
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-            """
-            cursor.execute(query, (user_id, username, today.year, today.month, today.day, today, no_off, is_sap))
-            conn.commit()
     except Exception as e:
         update.message.reply_text(f"Đã xảy ra lỗi: {e}")
 
